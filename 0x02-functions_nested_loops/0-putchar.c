00383b80e50492644851af190765ae4021ff3ce7
aduloju10 <christyaduloju@gmail.com>
2023-02-23 17:29:43 +0000

    Write a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *main - Main function
+ *Description: The function of the main
+ *_putchar: print the alphabats putchar
+ *Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
